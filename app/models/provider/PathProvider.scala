b314d72850848abc75fff82027fb6c375767c622
floriantopf <florian.topf@gmail.com>
2014-04-04 21:54:43 +0200

    simplified path provider

--- app/models/provider/PathProvider.scala
+++ app/models/provider/PathProvider.scala
@@ -4,17 +4,9 @@ package models.provider
 object PathProvider {
   def getPath(folder: String, dbname: String, path: String): String = {
     var name = path.split("/").last	
-    if(name.contains("?") || !name.toLowerCase().contains(dbname.toLowerCase())) {
-      if(folder == "trees") {
-        if(dbname == "AMDA") // @FIXME this is a hack!
-          name = folder.substring(0, folder.length()-1)+"_"+dbname+"_"+name.split("_").last
-        else
-          name = folder.substring(0, folder.length()-1)+"_"+dbname+".xml"
-        
-      }
-      else if(folder == "methods")
-        name = folder+"_"+dbname+".wsdl"
-    }
+    // just check if there is a GET route in methods
+    if(name.contains("?") && (folder == "methods"))
+        name = dbname+"_"+folder+".wsdl"    
     folder+"/"+dbname+"/"+name
   }
   
