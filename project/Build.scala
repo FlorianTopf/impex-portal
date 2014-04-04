c7793261e8c5ef6368a93d2890037295603976c6
floriantopf <florian.topf@gmail.com>
2014-04-05 01:31:35 +0200

    added settings for testing environment

--- project/Build.scala
+++ project/Build.scala
@@ -18,12 +18,16 @@ object ApplicationBuild extends Build {
     "net.databinder.dispatch" %% "dispatch-core" % "0.9.5",
     "org.scala-lang.modules" %% "scala-async" % "0.9.0-M4",
     "com.wordnik" %% "swagger-play2" % "1.3.4",
-    "com.wordnik" %% "swagger-play2-utils" % "1.3.4"
+    "com.wordnik" %% "swagger-play2-utils" % "1.3.4",
+    "org.scalatest" %%"scalatest" % "1.9.1" % "test",
+    "com.typesafe.akka" %% "akka-testkit" % "2.2.0" % "test",
+    "org.mockito" % "mockito-all" % "1.9.5"
   )
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
     // Add your own project settings here
-    scalacOptions ++= Seq("-feature")
+    scalacOptions ++= Seq("-feature"),
+    resolvers += "Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots"
   )
 /*
   val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA,
