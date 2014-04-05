a4d402e3c686afd98b13af0544405a2546848049
floriantopf <florian.topf@gmail.com>
2014-04-06 00:36:08 +0200

    some changes in the global startup object

--- app/Global.scala
+++ app/Global.scala
@@ -15,6 +15,7 @@ import java.io._
 object Global extends GlobalSettings {
   override def onStart(app: play.api.Application) {
 	import models.actor.ConfigService._
+	
     implicit val timeout = Timeout(10.seconds)
 
     val actor: ActorRef = Akka.system.actorOf(Props(new ConfigService), name = "config")
@@ -23,19 +24,22 @@ object Global extends GlobalSettings {
 
     Akka.system.actorOf(Props(new RegistryService), name = "registry")
  
-    initActors(databases)
-  }
+    initChildActors(databases)
+  } 
   
-  private def initActors(databases: Seq[Database]) {
+  private def initChildActors(databases: Seq[Database]) {
     databases map { database =>
       val dns: String = database.databaseoption.head.value
       val protocol: String = database.protocol.head
       val treeURLs: Seq[URI] = UrlProvider.getUrls(protocol, dns, database.tree)
       val methodsURLs: Seq[URI] = UrlProvider.getUrls(protocol, dns, database.methods)
-
+      
+      println("fetching files from "+database.name)
+      println("{")
       println("treeURL="+treeURLs)
       println("methodsURL="+methodsURLs)
-      println("fetching files from "+database.name)
+      println("}")
+
 
       val trees: Seq[NodeSeq] = fetchAndSaveFiles(treeURLs, "trees", database)
       val methods: Seq[NodeSeq] = fetchAndSaveFiles(methodsURLs, "methods", database)
@@ -52,15 +56,15 @@ object Global extends GlobalSettings {
   private def fetchAndSaveFiles(URLs: Seq[URI], folder: String, db: Database): Seq[NodeSeq] = { 
     URLs map { URL => 
       val fileName = PathProvider.getPath(folder, db.name, URL.toString)
-      // @TODO we do not download in DEV
+      // @TODO we do not download in DEVELOPMENT
 //		
 //      val fileDir = new File(folder+"/"+db.name)
 //      if(!fileDir.exists) fileDir.mkdir()
-//      
 //      val file = new File(fileName)
 //      if(!file.exists) file.createNewFile()
 //      
 //      val promise = WS.url(URL.toString).get()
+//      
 //      try {
 //        val result = Await.result(promise, 1.minute).xml
 //        scala.xml.XML.save(fileName, result, "UTF-8")
