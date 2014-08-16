18525d583c21818b3ca981b7c6b2551cc445802e
floriantopf <florian.topf@gmail.com>
2014-08-16 04:11:29 +0200

    remove unnecessary services

--- public/js/controllers/PortalCtrl.ts
+++ public/js/controllers/PortalCtrl.ts
@@ -11,15 +11,14 @@ module portal {
         private scope: portal.IPortalScope
         private timeout: ng.ITimeoutService
         private configService: portal.ConfigService
-        private registryService: portal.RegistryService
-        private userService: portal.UserService
         private state: ng.ui.IStateService
         private modal: any
         private growl: any
 
         public ready: boolean = false
       
-        static $inject: Array<string> = ['$scope', '$timeout', 'configService', '$state', '$modal', 'growl']
+        static $inject: Array<string> = ['$scope', '$timeout', 'configService', '$state', 
+            '$modal', 'growl']
 
         constructor($scope: IPortalScope, $timeout: ng.ITimeoutService, configService: portal.ConfigService, 
             $state: ng.ui.IStateService, $modal: any, growl: any) { 
@@ -35,6 +34,7 @@ module portal {
                   this.ready = true 
                   this.growl.warning('Configuration loaded, waiting for isAlive...')
             })
+          
             
         }
 
