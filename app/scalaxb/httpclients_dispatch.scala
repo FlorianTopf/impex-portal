c0caf86513cffcb4c7edfc1398bb8679a871a8a6
floriantopf <florian.topf@gmail.com>
2014-10-16 13:39:45 +0200

    introduced idle timeout of 2 minutes

--- app/scalaxb/httpclients_dispatch.scala
+++ app/scalaxb/httpclients_dispatch.scala
@@ -8,6 +8,7 @@ trait DispatchHttpClients extends HttpClients {
     // @FIXME maybe not working
     import dispatch.Defaults
     val http = new Http()
+    http.configure(_.setIdleConnectionTimeoutInMs(120000))
     
     def request(in: String, address: java.net.URI, headers: Map[String, String]): String = {
       val req = url(address.toString) << in <:< headers
